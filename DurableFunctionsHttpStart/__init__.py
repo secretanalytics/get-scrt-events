89e8c3543281aef167c8fd68872f08735cae1b3e
Duncan Ashe <d.ashe@cointel.ai>
2020-11-29 14:03:20 +0100

    httptriggerstart

---
+++ DurableFunctionsHttpStart/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+from azure.durable_functions as df 
+import azure.functions as func
+from ..shared_code.MyClasses import SerializableClass
+
+
+async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
+    client = df.DurableOrchestrationClient(starter)
+    instance_id = await client.start_new(req.route_params["functionName"], None, None)
+
+    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+
+    return client.create_check_status_response(req, instance_id)
